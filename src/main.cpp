b6ab877e75316187c28bd00cf1299e144728bcf1
Jonas Genannt <jonas@brachium-system.net>
2018-11-25 13:57:02 +0100

    update to NodeMCU

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "Arduino.h"
-#include <avr/power.h>
-#include <avr/sleep.h>
+#include <ESP8266WiFi.h>
 
-#include <SPI.h>
-#include <WiFi101.h>
+extern "C" {
+#include "user_interface.h"
+}
 
 #include "Shutter.h"
 #include "Light.h"
@@ -16,133 +16,118 @@ Shutter shutter3;
 Light light1;
 Light light2;
 
-/*
- * ISR should called every second
- */
-ISR(TIMER1_COMPA_vect) {
+os_timer_t Timer1;
+
+
+void timerCall(void*z) {
   shutter1.timer();
   shutter2.timer();
   shutter3.timer();
 }
 
 void setup_timer1(void) {
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
+  os_timer_setfn(&Timer1, timerCall, NULL);
+  os_timer_arm(&Timer1, 1000, true);
+}
 
-  /*
-   * 1/8000000 * 1024 = .00012800000000000000
-   * 1/.000128 = 7813
-   */
 
-  OCR1A = 7813;
-  TCCR1B |= _BV(WGM12);
-  TCCR1B |= _BV(CS10);
-  TCCR1B |= _BV(CS12);
-  TIMSK1 |= _BV(OCIE1A);
+void wificonnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("local IP:");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
-  ADCSRA &= ~(1 << 7);
-  power_twi_disable();
-  power_adc_disable();
-
-#ifdef DEBUG
   Serial.begin(9600);
   Serial.println("Starting Rollladen Steuerung");
-#else
-  power_usart0_disable();
-#endif
 
-  if (WiFi.status() == WL_NO_SHIELD) {
-#ifdef DEBUG
-    Serial.println("WiFi Module not found")
-#endif
-    while (true);
-  }
+  setup_timer1();
+  wificonnect();
 
   server.begin();
 
-  setup_timer1();
-  shutter1.begin(A1, A2, 20);
-  shutter2.begin(A3, A4, 20);
-  shutter2.begin(8, 9, 20);
+  shutter1.begin(D3, D4, 20);
+  shutter2.begin(D5, D6, 20);
+  shutter2.begin(D9, D10, 20);
 
-  light1.begin(A5);
-  light2.begin(10);
+  light1.begin(D1);
+  light2.begin(D2);
 }
 
 void loop() {
+  // Check if a client has connected
   WiFiClient client = server.available();
-  if (client) {
-    String header_line = "";
-    while (client.connected()) {
-      char c = client.read();
-      if (c == '\n') {
-        if (header_line.length() == 0) {
-          // request end
-          break;
-        }
-        else {
-          header_line = "";
-        }
-      }
-      else if (c != '\r') {
-        header_line += c;
-      }
-
-      // Light 1
-      if (header_line.endsWith("GET /light1/on")) {
-        light1.on();
-      }
-      if (header_line.endsWith("GET /light1/off")) {
-        light1.off();
-      }
-
-      // Light 2
-      if (header_line.endsWith("GET /light2/on")) {
-        light2.on();
-      }
-      if (header_line.endsWith("GET /light2/off")) {
-        light2.off();
-      }
-
-
-      // Shutter1
-      if (header_line.endsWith("GET /shutter1/down")) {
-        shutter1.down();
-      }
-      if (header_line.endsWith("GET /shutter1/up")) {
-        shutter1.up();
-      }
-      if (header_line.endsWith("GET /shutter1/halt")) {
-        shutter1.halt();
-      }
-
-
-      // Shutter2
-      if (header_line.endsWith("GET /shutter2/down")) {
-        shutter2.down();
-      }
-      if (header_line.endsWith("GET /shutter2/up")) {
-        shutter2.up();
-      }
-      if (header_line.endsWith("GET /shutter2/halt")) {
-        shutter2.halt();
-      }
-
-      // Shutter3
-      if (header_line.endsWith("GET /shutter3/down")) {
-        shutter3.down();
-      }
-      if (header_line.endsWith("GET /shutter3/up")) {
-        shutter3.up();
-      }
-      if (header_line.endsWith("GET /shutter3/halt")) {
-        shutter3.halt();
-      }
-    }
-
-    client.stop();
+  if (!client) {
+    return;
   }
+
+  // Wait until the client sends some data
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String header_line = client.readStringUntil('\r');
+  Serial.println(header_line);
+  client.flush();
+
+  // Light 1
+  if (header_line.startsWith("GET /light1/on")) {
+    light1.on();
+  }
+  if (header_line.startsWith("GET /light1/off")) {
+    light1.off();
+  }
+
+  // Light 2
+  if (header_line.startsWith("GET /light2/on")) {
+    light2.on();
+  }
+  if (header_line.startsWith("GET /light2/off")) {
+    light2.off();
+  }
+
+
+  // Shutter1
+  if (header_line.startsWith("GET /shutter1/down")) {
+    shutter1.down();
+  }
+  if (header_line.startsWith("GET /shutter1/up")) {
+    shutter1.up();
+  }
+  if (header_line.startsWith("GET /shutter1/halt")) {
+    shutter1.halt();
+  }
+
+  // Shutter2
+  if (header_line.startsWith("GET /shutter2/down")) {
+    shutter2.down();
+  }
+  if (header_line.startsWith("GET /shutter2/up")) {
+    shutter2.up();
+  }
+  if (header_line.startsWith("GET /shutter2/halt")) {
+    shutter2.halt();
+  }
+
+  // Shutter3
+  if (header_line.startsWith("GET /shutter3/down")) {
+    shutter3.down();
+  }
+  if (header_line.startsWith("GET /shutter3/up")) {
+    shutter3.up();
+  }
+  if (header_line.startsWith("GET /shutter3/halt")) {
+    shutter3.halt();
+  }
+
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nOK";
+  client.print(s);
+  delay(1);
 }
